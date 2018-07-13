2c6c471ec41840609eeadb6dde0bdff51f413e37
Shubham Singh <imshubhamsingh97@gmail.com>
2018-07-13 23:38:45 +0530

    logo

--- src/components/Sidebar/sideMenu.js
+++ src/components/Sidebar/sideMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Menu, Icon, Layout } from 'antd';
 
 import { Link, withRouter } from 'react-router-dom';
+import Logo from '../../assets/logo.svg';
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
@@ -12,15 +13,25 @@ class SideMenu extends Component {
   render() {
     console.log(this.props);
     var selectionMenu = '';
-    switch(this.props.location.pathname){
-      case "/home/seller" : selectionMenu = '1'; break;
-      case "/home/buyer" : selectionMenu = '2'; break;
-      case "/home/administration" : selectionMenu = '3'; break;
-      default: selectionMenu = '1';
+    switch (this.props.location.pathname) {
+      case '/home/seller':
+        selectionMenu = '1';
+        break;
+      case '/home/buyer':
+        selectionMenu = '2';
+        break;
+      case '/home/administration':
+        selectionMenu = '3';
+        break;
+      default:
+        selectionMenu = '1';
     }
 
     return (
       <Sider width={240} style={{ background: '#fff' }}>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <img src={Logo} style={{ width: 120 }} />
+        </div>
         <Menu style={{ width: 240, height: '100vh' }} selectedKeys={[selectionMenu]} defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline">
           <SubMenu
             key="sub1"
@@ -31,21 +42,15 @@ class SideMenu extends Component {
               </span>
             }
           >
-            <MenuItemGroup key="g1" title="Item 1">
+            <MenuItemGroup key="g1" title="Roles">
               <Menu.Item key="1">
-                <Link to="/home/seller">
-                  Seller
-                </Link>
+                <Link to="/home/seller">Seller</Link>
               </Menu.Item>
               <Menu.Item key="2">
-                <Link to="/home/buyer">
-                  Buyer
-                </Link>
+                <Link to="/home/buyer">Buyer</Link>
               </Menu.Item>
               <Menu.Item key="3">
-                <Link to="/home/administration">
-                  Administration
-                </Link>
+                <Link to="/home/administration">Administration</Link>
               </Menu.Item>
             </MenuItemGroup>
             <MenuItemGroup key="g2" title="Item 2">
